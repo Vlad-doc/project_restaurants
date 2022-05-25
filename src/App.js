5e27c2f23db4e1473685e1ffe47e6fce409ba841
Your Name <you@example.com>
2022-05-25 11:56:00 +0300

    add basic router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import Cart from "./components/cart"
 import Filter from "./components/filter"
 import OrderForm from "./components/order-form"
 import RestaurantsList from "./components/restaurants-list"
+import { Route, NavLink } from "react-router-dom"
 
 function App() {
   return (
     <div>
       <h2>Restaurants list</h2>
-      <Filter />
-      <RestaurantsList />
-      <OrderForm />
-      <Cart />
+      <NavLink to="/checkout">
+        <Cart />
+      </NavLink>
+      <NavLink to="/restaurants">Restaurants</NavLink>
+      <NavLink to="/filter">Filter</NavLink>
+
+      <Route path="/filter" component={Filter} />
+      <Route path="/restaurants" component={RestaurantsList} />
+      <Route path="/checkout" component={OrderForm} />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from "./App"
 import "antd/dist/antd.css"
 import { Provider } from "react-redux"
 import store from "./store"
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>,
 )
