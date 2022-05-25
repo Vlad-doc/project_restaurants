06fea5add7df6f03451d69e75d777ae407a3eefd
Your Name <you@example.com>
2022-05-25 14:54:11 +0300

    add Switch and exact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from "react"
+import { Route, NavLink, Switch } from "react-router-dom"
+import "antd/dist/antd.css"
+import OrderForm from "./components/order-form"
 import Cart from "./components/cart"
 import Filter from "./components/filter"
-import OrderForm from "./components/order-form"
 import RestaurantsPage from "./components/routes/restaurants"
-import { Route, NavLink } from "react-router-dom"
 
-function App() {
+export default function App() {
   return (
     <div>
-      <h2>Restaurants list</h2>
+      <h1>Delivery App</h1>
       <NavLink to="/checkout">
         <Cart />
       </NavLink>
       <NavLink to="/restaurants">Restaurants</NavLink>
       <NavLink to="/filter">Filter</NavLink>
 
-      <Route path="/filter" component={Filter} />
-      <Route path="/restaurants" component={RestaurantsPage} />
-      <Route path="/checkout" component={OrderForm} />
+      <Switch>
+        <Route path="/filter" exact component={Filter} />
+        <Route
+          path="/restaurants/:id/review"
+          render={({ id }) => <h1>Add a review for {id}</h1>}
+        />
+        <Route path="/restaurants" component={RestaurantsPage} />
+        <Route path="/checkout" component={OrderForm} />
+      </Switch>
     </div>
   )
 }
-
-export default App
